9327f2ede0a7b86660529ec73c2f02a9f2fc3ca1
AnishRajwani <anish.rajwani@whiteklay.in>
2024-12-31 17:48:58 +0530

    updated database uri dynamic

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+load_dotenv()
 
-uri = "mysql+pymysql://root:@localhost/placement_drive"
+uri = os.getenv("db")
 
 engine = create_engine(uri)
 
